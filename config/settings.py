74d5866e82aaa4822db1b705eb2ba56dc262b405
RICHARDAVF <richarferro1308@gmail.com>
2025-06-15 22:06:21 -0500

    cambios

--- config/settings.py
+++ config/settings.py
@@ -137,11 +137,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
+STATIFILES_DIR = [
+    os.path.join(BASE_DIR,'static')
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #AUTHENTICATION
